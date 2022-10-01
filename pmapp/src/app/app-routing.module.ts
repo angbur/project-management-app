214f6be87289081bfbde027fd15b1c7326224678
angbur <41025473+angbur@users.noreply.github.com>
2022-10-01 12:25:38 +0200

    feat: add first-section component

--- pmapp/src/app/app-routing.module.ts
+++ pmapp/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { RegisterComponent } from './auth/components/register/register.component';
+import { HomePageComponent } from './home-page/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'sign-up', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'dashboard',
+    loadChildren: ()=> import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+  { path: 'profile',
+    loadChildren: ()=> import('./profile/profile.module').then(m => m.ProfileModule)
+  },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
