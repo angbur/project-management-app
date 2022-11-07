71066c0ad5a8ccdf9178c40362e86c562288978f
angbur <ag.grala@gmail.com>
2022-11-07 10:45:47 +0100

    bugs: add missing file

---
+++ pmapp/src/app/shared/components/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { logout } from './../../../state/system/system.actions';
+import { selectLoginStatus } from './../../../state/index';
+import { select, Store } from '@ngrx/store';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SystemState } from 'src/app/state/system/system.reducer';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  isLoggedIn: boolean = false;
+  userName = '';
+
+  constructor(private router: Router, private readonly store: Store<SystemState>) {
+    this.store.pipe(select(selectLoginStatus)).subscribe(status => (this.isLoggedIn = status));
+  }
+
+  handleLogout(): void {
+    this.store.dispatch(logout());
+  }
+
+  navigateToProfilePage(): void {
+    this.router.navigate(['/profile']);
+  }
+
+  navigateToDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
+}
